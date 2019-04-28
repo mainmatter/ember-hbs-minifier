1bcc22e67ffd0c17610345866fff6ebb39c9c3dc
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-04-28 15:21:10 +0200

    Remove unnecessary `AttrNode` check
    `attributes` are always `AttrNode` instances

--- hbs-minifier-plugin.js
+++ hbs-minifier-plugin.js
@@ -129,7 +129,7 @@ function stripNoMinifyBlocks(nodes) {
 
 function getElementAttribute(node, attrName) {
   let attribute = node.attributes.find((attr) => {
-    return attr.type === 'AttrNode' && attr.name === attrName;
+    return attr.name === attrName;
   });
   return (attribute || {}).value;
 }
