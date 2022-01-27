6727dd7cc999d4cbc62457f2dd2cef207d2d8b2e
Tobias Bieniek <tobias.bieniek@gmail.com>
2022-01-27 10:28:01 +0100

    ember-try: Disable Ember.js 4.x scenarios
    until we actually fix them...

--- config/ember-try.js
+++ config/ember-try.js
@@ -3,4 +3,8 @@ module.exports = {
   command: 'yarn run ember test',
   useVersionCompatibility: true,
   useYarn: true,
+  scenarios: [{
+    name: 'ember-release',
+    allowedToFail: true,
+  }]
 };
